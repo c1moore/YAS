04adc0958f5d9f3ef22178171942eccfc00384e8
Calvin Moore <c1moore@ufl.edu>
2015-03-17 13:33:21 -0400

    Added an additional field for command table entries for error, define constants to use for standard input/output/error, and made a global variable to count the number of commands.

--- includes/yas.h
+++ includes/yas.h
@@ -10,6 +10,14 @@
 	#define BG_MODE_FALSE	0
 
 	/**
+	* The values to enter for C_INPUT and C_OUTPUT when standard input,
+	* standard output, and standard error should be used.
+	*/
+	#define YAS_STDIN		-1
+	#define YAS_STOUT		-2
+	#define YAS_STDERR		-3
+
+	/**
 	* Define the type to be used for C_INPUT and C_OUTPUT.  Since I/O for a command can
 	* be either a filename (char *), a pointer to another command (int), or STDIN or
 	* STDOUT (int); we will use a union of these types.
@@ -29,9 +37,15 @@
 		char *C_ARGS_PNTR[INIT_ARGS];			//Pointer to each argument in C_ARGS.
 		union C_IO_TYPE C_INPUT;				//Input for the command
 		union C_IO_TYPE C_OUTPUT;				//Output for the command
+		union C_IO_TYPE C_ERR;					//Error for the command
 	};
 
 	struct cmd cmdtab[CMDS_MAX];				//Table of commands
+
+	/**
+	* Number of commands in the command table.
+	*/
+	int num_cmds = 0;
 
 	/**
 	* Specifies whether this command should run in the background.  If its value is
