d06bf245b910799c3ef5e0f727561edd75182385
dalton <daltonverhagen@gmail.com>
2015-04-01 11:43:02 -0400

    added header file for built in commands

---
+++ includes/builtin_commands.h
@@ -0,0 +1,10 @@
+#ifndef _YAS_CMDS
+	#define _YAS_CMDS
+	
+	int alias(int argc, char *argv[]);
+	int cd(int argc, char *argv[]);
+	int printenv(int argc, char *argv[]);
+	int setenv(int argc, char *argv[]);
+	int unsetenv(int argc, char *argv[]);
+
+#endif
