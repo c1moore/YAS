c30d2f342bde160a3784d176937e6ca87eec8b37
dalton <daltonverhagen@gmail.com>
2015-04-11 12:07:38 -0400

    removed the help command for now

---
+++ commands/help.c
@@ -0,0 +1,24 @@
+#include <syscall.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <yas.h>
+
+int help(int argc, char *argv[]) {
+	/*int cd;
+	char *cmds[] = {"alias bye cd printenv setenv unalias unsetenv"};
+	//if cd has no arguments go to the home dir else go to the directort in the argument
+	if(argc == 2) {
+		cd = chdir(getenv("HOME"));
+	}
+	else {
+		printf("Select a command to get help on:\n");
+		while(cmds) {
+			printf("%s\n");
+			cmds++
+		}
+		getch();
+	}
+	*/
+}
