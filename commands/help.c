83227f4d20c30f6cf04d5c4e9a6599bf0a19ee57
dalton <daltonverhagen@gmail.com>
2015-04-13 13:14:07 -0400

    fixed an extra white space in the help.c text

--- commands/help.c
+++ commands/help.c
@@ -9,33 +9,40 @@
 and the user can select one to see the command usage.*/
 int help(int argc, char *argv[]) {
 	int cd;
-	char * cmds[] = {"1. alias", "2. bye", " 3. cd", "4. printenv", "5. setenv", "6. unalias", "7. unsetenv"};
+	char * cmds[] = {"1. alias", "2. bye", "3. cd", "4. printenv", "5. setenv", "6. unalias", "7. unsetenv"};
 	int cmdsLength = sizeof(cmds)/sizeof(const char *);
 	//if cd has no arguments go to the home dir else go to the directort in the argument
 	if(argc) {
 		int cmd;
-		printf("Select a command to get help on:\n");
 		int i;
 		for(i=0;i<cmdsLength;i++) {
 			printf("%s\n",cmds[i]);
 		}
+		printf("Select a command to get help on: ");
 		scanf("%d",&cmd);
 		printf("Command usage:\n");
 		switch (cmd) {
 			case 1:
-				printf("alias [alias name] [command]\n");
+				printf("alias [alias name] [command]");
+				break;
 			case 2:
 				printf("bye");
+				break;
 			case 3:
-				printf("cd [directory]\n");
+				printf("cd [directory]");
+				break;
 			case 4:
 				printf("printenv");
+				break;
 			case 5:
 				printf("setenv [ENV Name] [Variable]");
+				break;
 			case 6:
 				printf("unalias [alias name]");
+				break;
 			case 7:
 				printf("unsetenv [alias name]");
+				break;
 		}
 		return(0);
 	}
